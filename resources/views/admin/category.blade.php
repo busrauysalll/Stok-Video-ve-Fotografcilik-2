5b0c281ae1c71c0e5fab391743b3690efe994c41
busrauysalll <bsryslbsrysl1995@gmail.com>
2021-04-08 15:06:53 +0300

    Added new line

---
+++ resources/views/admin/category.blade.php
@@ -0,0 +1,85 @@
+@extends('layouts.admin')
+
+@section('title', 'Category List')
+
+
+@section('content')
+
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Categories</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active">Category Page</li>
+                        </ol>
+                    </div>
+                </div>
+            </div><!-- /.container-fluid -->
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+
+            <!-- Default box -->
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Category List</h3>
+                </div>
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">DataTable with default features</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+                        <table id="example1" class="table table-bordered table-striped">
+                            <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Parent</th>
+                                <th>Title</th>
+                                <th>Status</th>
+                                <th>Edit</th>
+                                <th>Delete</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+
+                            @foreach ($datalist as $rs)
+                          <tr>
+                                <td>{{ $rs->id }}</td>
+                                <td>{{ $rs->parent_id }}</td>
+                                <td>{{ $rs->title }}</td>
+                                <td>{{ $rs->status }}</td>
+                                <td>Edit</td>
+                                <td>Delete</td>
+                            </tr>
+                            @endforeach
+
+                        </table>
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+
+                <!-- /.card-body -->
+                <div class="card-footer">
+                    Footer
+                </div>
+                <!-- /.card-footer-->
+            </div>
+            <!-- /.card -->
+
+        </section>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+
+@endsection
